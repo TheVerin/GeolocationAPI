a15079923a77f1c220bc017146ad412c29881311
TheVerin <tomekbeben95@gmail.com>
2020-01-11 13:28:18 +0100

    Add filtering to location viewset

--- core/settings/base.py
+++ core/settings/base.py
@@ -46,6 +46,12 @@ SITE_ID = 1
 
 REST_USE_JWT = True
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+            'rest_framework.permissions.AllowAny',
+        )
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
--- core/settings/dev.py
+++ core/settings/dev.py
@@ -4,9 +4,3 @@ from .base import *
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.AllowAny',
-    )
-}
