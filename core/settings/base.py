d447140897c1553a5eedd55512d31d640c8a18c3
TheVerin <tomekbeben95@gmail.com>
2020-01-11 14:53:23 +0100

    Add flake8 and refactor for lint

--- core/settings/base.py
+++ core/settings/base.py
@@ -1,5 +1,4 @@
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -10,6 +9,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 IPSTACK_KEY = os.environ.get('IPSTACK_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
 
 
 # Application definition
